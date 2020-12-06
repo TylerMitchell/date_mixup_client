4e72767a755be0e74c75f6732228ca8cc19a6696
Tyler Mitchell <tylermitchellisawesome@gmail.com>
2020-12-06 11:16:54 -0500

    changing websocket server url

--- src/helpers/environment.tsx
+++ src/helpers/environment.tsx
@@ -4,11 +4,11 @@ let WS_APIURL = "";
 switch(window.location.hostname){
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:4000';
-        WS_APIURL = 'ws://localhost:4001';
+        WS_APIURL = 'wss://localhost:4001';
         break;
     case 'date-mixup.herokuapp.com':
         APIURL = "https://date-mixup-server.herokuapp.com";
-        WS_APIURL = "https://date-mixup-server.herokuapp.com"; //I might need to make a separate WS Server!
+        WS_APIURL = "https://date-mixup-wsserver.herokuapp.com"; //I might need to make a separate WS Server!
         break;
     default:
         console.log("The hostname encountered in environment.tsx was not expected!");
